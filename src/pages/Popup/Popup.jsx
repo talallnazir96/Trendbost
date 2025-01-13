fe3a441d32b82358f0477df1a568c73e3fd6aa09
Muhammad Ahsan Ehsan <ahsanehsan00@gmail.com>
2025-01-13 16:25:31 +0500

    changes in style

--- src/pages/Popup/Popup.jsx
+++ src/pages/Popup/Popup.jsx
@@ -28,7 +28,7 @@ const Popup = () => {
             height="50"
             className="tick-animate"
           >
-            <circle cx="12" cy="12" r="12" fill="#4CAF50" />
+            <circle cx="12" cy="12" r="12" fill="#6a0dad" />
             <path
               fill="white"
               d="M9.707 16.707L5.293 12.293 6.707 10.879 9.707 13.879 17.293 6.293 18.707 7.707z"
